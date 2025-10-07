98e6ffa7d5edd2c32ceb85c01529144d1345148a
Pavel <ltsmilik@gmail.com>
2025-10-07 17:57:04 +0500

    New StringSummerClass

--- src/main/java/org/machine/Main.java
+++ src/main/java/org/machine/Main.java
@@ -1,7 +1,15 @@
 package org.machine;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        Printer.work();
+
+        String s1="",s2="";
+        Scanner input = new Scanner(System.in);
+        s1 = input.nextLine();
+        s2= input.nextLine();
+        System.out.println(StringSummer.sum(s1,s2));
+        //Printer.work();
     }
 }
